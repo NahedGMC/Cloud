b2776068b8350a9477fa422138d094287e5aafae
Nahed <khechainianahed22@gmail.com>
2023-06-20 18:25:12 +0100

    first commit

---
+++ cloud/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import MapContainer from './MapContainer';
+
+function App() {
+  return (
+    <div className="App">
+     <MapContainer></MapContainer>
+    </div>
+  );
+}
+
+export default App;
